b91b201d79c1cba6943e5f7277e87538d029f5f9
krjarnold <krjarnold@gmail.com>
2017-02-02 14:54:23 -0500

    Fix migration

--- db/migrate/20170131200146_change_products.rb
+++ db/migrate/20170131200146_change_products.rb
@@ -1,7 +1,6 @@
 class ChangeProducts < ActiveRecord::Migration
   def change
     change_table  :products do |t|
-      t.remove :product_id
       t.text :description
     end
   end
--- db/migrate/20170131212809_add_to_products.rb
+++
@@ -1,7 +0,0 @@
-class AddToProducts < ActiveRecord::Migration
-  def change
-    change_table  :products do |t|
-      t.integer :product_id
-    end
-  end
-end
